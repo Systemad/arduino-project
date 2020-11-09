dc46e37c5384ebf7ed8ccf1c8a59ba5197970a0d
systemad <dannesuke@gmail.com>
2020-11-09 12:04:55 +0100

    Add makefile and basic main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <stdio.h>
+#include <string.h>
+#include <util/delay.h>
+
+void main(void)
+{
+
+    while (1)
+    {
+    }
+}
